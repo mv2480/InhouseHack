57c4cfacca4a1fecd266353a00540e608c636e45
mv2480 <60354122+mv2480@users.noreply.github.com>
2021-02-14 12:06:22 +0530

    Add files via upload

---
+++ Backend Integration.py
@@ -0,0 +1,26 @@
+from pydrive.auth import GoogleAuth
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+
+drive = GoogleDrive(gauth)
+
+file1 = drive.CreateFile({"Vadhiraj_Activity": "text/csv"})
+file1.SetContentFile("Vadhiraj_Activity")
+file1.Upload()
+
+file2 = drive.CreateFile({"Sarthak_Activity": "text/csv"})
+file2.SetContentFile("Sarthak_Activity")
+file2.Upload()
+
+file3 = drive.CreateFile({"Mridul_Activity": "text/csv"})
+file3.SetContentFile("Mridul_Activity.csv")
+file3.Upload()
+
+file4 = drive.CreateFile({"Soham_Activity": "text/csv"})
+file4.SetContentFile("Soham_Activity.csv")
+file4.Upload()
+
+file1 = drive.CreateFile({"Attendance_Stats": "text/csv"})
+file1.SetContentFile("Attendance_Stats.csv")
+file1.Upload()
